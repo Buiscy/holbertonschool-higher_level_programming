cdb3c0b6213ccb79091a3f74cfc856862b75a71c
Buiscy <Aaron.regterschot@gmail.com>
2026-05-17 01:29:48 +1000

    Push through

--- python-import_modules/Task-4.py
+++ python-import_modules/Task-4.py
@@ -4,4 +4,4 @@ if __name__ == "__main__": #Stop it from actioning on load
     names = dir(hidden_4) #names is the variable. dir() returns all the modules in the file. E.G dir(hidden_4) here returns any modules inside that file.
     for name in sorted(names): #sorted function sorts by default alphabetically
         if not name.startswith("__"): #filtering out any modules that start with __. startswith() is a function that checks the start of a file name
-            print(name)
+            print(name)#

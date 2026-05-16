a978aa9910af7a81ac47409ada5bab39aa857a91
Buiscy <Aaron.regterschot@gmail.com>
2026-05-16 20:41:06 +1000

    Think I'm done

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import sys
-if __name__ == '__main__'
-    i = len(sys.argv) - 1
-    b = -1
-    print('Arguments:{}'.format(i))
-    for b in range(i):
-        print(i)
+from sys import argv
+if __name__ == '__main__':
+    i = len(argv)
+    print('Arguments:{}'.format(i - 1))
+    for i in range(1, len(argv)):
+        print("{:d}: {}".format(i, argv[i]))
 
 #for i in range(1, sys.argv):
 #   print(i, sys.argv[i])

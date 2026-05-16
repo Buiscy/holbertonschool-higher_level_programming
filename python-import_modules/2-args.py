5e97af4d02574373d9a1e3fd72ef1402e6af62b2
Buiscy <Aaron.regterschot@gmail.com>
2026-05-16 21:29:03 +1000

    index fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
+if __name__ == '__main__':
+    from sys import argv
     argc = len(argv) - 1
-    if (argc == 0):
-        print(f"{argc} arguments.")
-    elif (argc == 1):
-        print(f"{argc} argument:")
+    if argc == 0:
+        print('0 arguments.')
+    elif argc == 1:
+        print('1 argument:')
     else:
-        print(f"{argc} arguments:")
-    for arg in range(1, argc + 1):
-        print(f"{arg}: {argv[arg]}")
-
-
-
-
-
-#from sys import argv
-#if __name__ == '__main__':
-#    i = len(argv)
-#    print('Arguments:{}'.format(i - 1))
-#    for i in range(1, len(argv)):
-#        print("{:d}: {}".format(i, argv[i]))
+        print('{} arguments:'.format(argc))
+    for index in range(1, len(argv)):
+        print("{:d}: {}".format(index, argv[index]))

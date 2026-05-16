f0fac993ad469e072fcec85fc4bec3a42f1fd1ac
Buiscy <Aaron.regterschot@gmail.com>
2026-05-16 20:24:08 +1000

    Learning Args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+i = len(sys.argv) - 1
+b = -1
+print('Arguments:{}'.format(i))
+for b in range(i):
+    print(i)
+
+#for i in range(1, sys.argv):
+#   print(i, sys.argv[i])

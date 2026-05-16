d6b40367ab8a7a11433c7c4d064721672855c66c
Buiscy <Aaron.regterschot@gmail.com>
2026-05-16 20:31:25 +1000

    fixed import issue

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
-i = len(sys.argv) - 1
-b = -1
-print('Arguments:{}'.format(i))
-for b in range(i):
-    print(i)
+if __name__ == '__main__'
+    i = len(sys.argv) - 1
+    b = -1
+    print('Arguments:{}'.format(i))
+    for b in range(i):
+        print(i)
 
 #for i in range(1, sys.argv):
 #   print(i, sys.argv[i])

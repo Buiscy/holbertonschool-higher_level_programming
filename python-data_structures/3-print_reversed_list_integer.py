7b8cf0d26c8ed3c69ecc57e24fea4e038e848c08
Buiscy <Aaron.regterschot@gmail.com>
2026-05-17 22:40:00 +1000

    pystyle fixes

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-#the three -1's are, in order: Start point, stop point, incriment by.
+    # the three -1's are, in order: Start point, stop point, incriment by.
     for index in range(len(my_list) - 1, - 1, - 1):
         print('{:d}'.format(my_list[index]))

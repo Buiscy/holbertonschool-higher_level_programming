98f1c685db417f7c7500b5feaf67a6b9a72fada6
Buiscy <Aaron.regterschot@gmail.com>
2026-05-17 21:43:54 +1000

    Pushing some learning

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    else:
+        #makes a copy
+        new_in_list = my_list.copy()
+        #modifies the element at index (idx) of the copied list
+        new_in_list[idx] = element
+        #returns the copy
+        return (new_in_list)

9d58eb72d811d5db20fbdcec38f57926f5d6195d
Buiscy <Aaron.regterschot@gmail.com>
2026-05-24 23:44:21 +1000

    Another commit

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = (tu)
-    return (i)
+    def pad (t):    #takes in a tuple, and pads out each of the two indexs if they dont exist
+        i = t[0] if len(t) > 0 else 0
+        j = t[1] if len(t) > 1 else 0
+        return (i, j) # returns each index
+    a = pad(tuple_a)
+    b = pad(tuple_b)
+    
+    return (a[0] + b[0], a[1] + b[1])

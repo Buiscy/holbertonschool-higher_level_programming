744fd416c5c08b6e8718cbf3f1252101de0ddd9d
Buiscy <Aaron.regterschot@gmail.com>
2026-05-24 17:06:39 +1000

    Added the comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''A class that can hold the value 'size' '''
+
 class Square:
+    '''A class with the value 'size' inside it'''
+    
     def __init__(self, size):
         self.__size = size
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''A class that can hold the value 'size' '''
+
 class Square:
+    '''A class with the value 'size' inside it'''
+
     def __init__(self, size=0):
             self.size = size
 

4fcb3ab98fdf9425afbb2dc9f5407a2a30f1b481
Buiscy <Aaron.regterschot@gmail.com>
2026-05-31 23:41:25 +1000

    fixes

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+'''So this tests if things are same class'''
 def is_kind_of_class(obj, a_class):
+    '''So this tests if things are same class'''
     return True if isinstance(obj, a_class) else False
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,2 +1,5 @@
+#!/usr/bin/python3
+'''So this tests if things are same class'''
 def inherits_from(obj, a_class):
+    '''So this tests if things are same class'''
     return True if isinstance(obj, a_class) and type(obj) is not a_class else False

9e62b3c7fe7ee865e7868ddcdb1db54fd51611b9
Buiscy <Aaron.regterschot@gmail.com>
2026-06-04 02:28:24 +1000

    PEP fix

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """write file module"""
 
+
 def write_file(filename="", text=""):
+
     """write file module"""
     with open(filename, "w", encoding="utf-8") as f:
         return(f.write(text))

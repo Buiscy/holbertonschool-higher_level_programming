1123a45eacd2d5c6095d72ca82c0cc39838b4f70
Buiscy <Aaron.regterschot@gmail.com>
2026-06-07 23:51:10 +1000

    Comit

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Json file module"""
+import json
+
+def from_json_string(my_str):
+    """Json file module read"""
+    return(json.loads(my_str))
---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Json file module"""
+import json
+
+def save_to_json_file(my_obj, filename):
+    """Json file module read"""
+    return(json.loads(my_str))
